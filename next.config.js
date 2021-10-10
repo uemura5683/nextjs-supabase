957ae9e9d517ef2382cb40311eacfc3224e89cca
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-10-10 11:29:22 +0900

    アカウント情報を全てenv管理する

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    SUPABASE_URL: "https://sqzjoytuljncpyoxmpuf.supabase.co",
-    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzAxNzIzNSwiZXhwIjoxOTQyNTkzMjM1fQ.gJ4_He7WidWwTfrMlnelU1Fi8Rb267uKOv052o-W_mo",
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
   },
 }
